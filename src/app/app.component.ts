e370d79579a5199f0e734456d4cadea95ebbda48
HUY_TRAN <huytranprers@gmail.com>
2018-06-21 23:02:56 +0700

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  questionModel = {
+    question: `When do you .... college?`,
+    answer: {
+      a: `Option 1`,
+      b: `Option 2`,
+      c: `Option 3`,
+      d: `Option 4`,
+    },
+    rightAnswer: `a`
+  };
+  show = null;
+  checkAnswer(answer) {
+    if (answer === this.questionModel.rightAnswer) {
+      console.log('right');
+      this.show = true;
+    } else {
+      console.log('wrong');
+      this.show = false;
+    }
+  }
 }
